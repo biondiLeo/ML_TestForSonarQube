df526973123892c96e4a48cc31695925cd5d6ac3
biondiLeo <33348005+biondiLeo@users.noreply.github.com>
2023-07-17 17:46:50 +0200

    Add files via upload

---
+++ s2225.java
@@ -0,0 +1,19 @@
+public class ExampleClass {
+    private String name;
+
+    public ExampleClass(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return null;
+    }
+
+    // Altri metodi...
+}
