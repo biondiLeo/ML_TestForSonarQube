0d859804c0b21158a915f479999c405e9126a530
biondiLeo <33348005+biondiLeo@users.noreply.github.com>
2023-07-17 16:39:12 +0200

    Create s1854.java

---
+++ s1854.java
@@ -0,0 +1,14 @@
+public class s1854 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int x = a + b;
+        int y = b - a;
+        int z = x * y;
+
+        System.out.println("utilizzato variabile x di valore: "+ x);
+    
+    }
+}
