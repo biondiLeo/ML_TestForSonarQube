44281e95fc677b2b8d8c1b6a461e156e4386df51
biondiLeo <33348005+biondiLeo@users.noreply.github.com>
2023-07-18 16:47:55 +0200

    Update s2225_difficult.java

--- s2225_difficult.java
+++ s2225_difficult.java
@@ -1,10 +1,12 @@
-public String toString() {
-   Random r = new Random();
-   if(r.nextInt(10) == r.nextInt(10)){
-       return null; // Noncompliant
+public class ExampleClass {
+   public String toString() {
+      Random r = new Random();
+      if(r.nextInt(10) == r.nextInt(10)){
+          return null; // Noncompliant
+      }
+      else if(r.nextInt(10) == r.nextInt(10)){
+         return "null";
+      }
+      return "";
    }
-   else if(r.nextInt(10) == r.nextInt(10)){
-      return "null";
-   }
-   return "";
 }
