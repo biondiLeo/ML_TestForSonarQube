e5adedda3bc7d3eb2a2cb7ab77d65d48539a4894
biondiLeo <33348005+biondiLeo@users.noreply.github.com>
2023-07-18 15:46:30 +0200

    Update s1155.java

--- s1155.java
+++ s1155.java
@@ -18,28 +18,51 @@ public class CollectionChecks {
         
         collectionChecks.addItem("Elemento 1");
         collectionChecks.addItem("Elemento 2");
-
-        if (0 == collectionChecks.myCollection.size()) {
+        
+        //== 0
+        if (collectionChecks.myCollection.size() == 0) 
+        {
             System.out.println("La collezione è vuota");
         }
 
-        if (collectionChecks.myCollection.size() == 0) {
+        //0 ==
+        if (0 == collectionChecks.myCollection.size()) 
+        {
             System.out.println("La collezione è vuota");
         }
         
-        if (0 != collectionChecks.myCollection.size()) {
+        //0 !=
+        if (0 != collectionChecks.myCollection.size()) 
+        {
             System.out.println("La collezione non è vuota");
         }
 
-        if (collectionChecks.myCollection.size() != 0) {
+        //!= 0
+        if (collectionChecks.myCollection.size() != 0) 
+        {
             System.out.println("La collezione non è vuota");
         }
 
+        //< 1
         if (collectionChecks.myCollection.size() < 1) {
             System.out.println("La dimensione della collezione è inferiore a 1");
         }
+
+        // 1 >
+        if (1 > myCollection.size()) 
+        {
+            System.out.println("La dimensione della collezione è inferiore a 1");
+        }
         
-        if (0 >= collectionChecks.myCollection.size()) {
+        //0 >=
+        if (0 >= collectionChecks.myCollection.size()) 
+        {
+            System.out.println("La dimensione della collezione è maggiore o uguale a 0");
+        }
+
+        // <= 0
+        if (myCollection.size() <= 0) 
+        {
             System.out.println("La dimensione della collezione è maggiore o uguale a 0");
         }
     }
