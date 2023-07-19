a21ce6b90640c968e6adce642e7dc63f34f60907
biondiLeo <33348005+biondiLeo@users.noreply.github.com>
2023-07-19 09:33:54 +0200

    Create s1854_easy.java

---
+++ s1854_easy.java
@@ -0,0 +1,11 @@
+public void dead()
+{
+      int x=5;
+      int y=10;
+      int z;
+      y=x*x*y;
+      System.out.println(y);
+      x=5;  // dead store
+      y=10; // dead store
+      z=20; // dead store
+ }
