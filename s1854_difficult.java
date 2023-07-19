fac20e123b9ec1b03c6f11db83f7ee69d6f46503
biondiLeo <33348005+biondiLeo@users.noreply.github.com>
2023-07-19 15:59:41 +0200

    Create s1854_difficult.java

---
+++ s1854_difficult.java
@@ -0,0 +1,14 @@
+public class test2 {
+    public void dead(int a, int b) {
+        int c = 2;
+        if (a < b) {
+           int c;
+            if (b < a) {
+                c = a + b;
+            } else {
+                c = a - b;
+            }
+            System.out.println(c);
+        }
+    }
+}
