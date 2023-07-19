62c6480bbb20195c8019ee49a71b817bfc6b3acb
biondiLeo <33348005+biondiLeo@users.noreply.github.com>
2023-07-19 14:53:42 +0200

    Update s1854_easy.java

--- s1854_easy.java
+++ s1854_easy.java
@@ -1,12 +1,14 @@
-public void dead()
-{
-      int x=5;
-      int y=10;
-      int z;
-      int w; 
-      y=x*x*y;
-      System.out.println(y);
-      x=5;  // dead store
-      y=10; // dead store
-      z=20; // dead store
- }
+public class test {    
+      public void dead()
+      {
+            int x=5;
+            int y=10;
+            int z;
+            int w; 
+            y=x*x*y;
+            System.out.println(y);
+            x=5;  // dead store
+            y=10; // dead store
+            z=20; // dead store
+       }
+}
