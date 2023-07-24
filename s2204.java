d1c4aed05ad918bdb9cad0fe925db3d47ec8427c
biondiLeo <33348005+biondiLeo@users.noreply.github.com>
2023-07-24 15:45:02 +0200

    Update s2204.java

--- s2204.java
+++ s2204.java
@@ -1,8 +1,8 @@
- public class AtomicClassEqualsInvocation {
+public class AtomicClassEqualsInvocation {
 
-     public void compute() {
-        AtomicInteger aInt1 = new AtomicInteger(0);
- 		    AtomicInteger aInt2 = new AtomicInteger(0);
-    		isEqual = aInt1.equals(aInt2); // Noncompliant
-     }
- }
+   public void compute() {
+    AtomicInteger aInt1 = new AtomicInteger(0);
+    AtomicInteger aInt2 = new AtomicInteger(0);
+    isEqual = aInt1.equals(aInt2); // Noncompliant
+   }
+}
