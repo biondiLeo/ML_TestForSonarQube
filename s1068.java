43cb366f418f432fe5a62e24e879023b4b77bf2e
biondiLeo <33348005+biondiLeo@users.noreply.github.com>
2023-07-24 11:31:51 +0200

    Update s1068.java

--- s1068.java
+++ s1068.java
@@ -1,7 +1,9 @@
  public class UnusedPrivateField {
      private String a = "Hello world!";
+     private int s, t, l;
 
      public int compute(int number) {
          return number * 42;
+         this.s = number + 20 + this.t;
      }
  }
